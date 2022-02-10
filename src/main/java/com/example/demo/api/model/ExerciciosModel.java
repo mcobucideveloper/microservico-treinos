bd235c0b9bcab2a81f2d8d32439a349b3b1cc3b1
mcobucideveloper <mcobuci.developer@gmail.com>
2022-02-10 19:42:12 -0300

    buscartodos

--- src/main/java/com/example/demo/api/model/ExerciciosModel.java
+++ src/main/java/com/example/demo/api/model/ExerciciosModel.java
@@ -9,4 +9,20 @@ public class ExerciciosModel {
 	private Long id;
 	private String descricao;
 
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return this.descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
 }
--- src/main/java/com/example/demo/infrastructure/repository/ExerciciosRepository.java
+++ src/main/java/com/example/demo/infrastructure/repository/ExerciciosRepository.java
@@ -6,20 +6,9 @@ import com.example.demo.domain.model.Exercicios;
 
 import org.springframework.stereotype.Repository;
 
-
-
 @Repository
 public interface ExerciciosRepository extends  CustomJpaRepository<Exercicios, Long> {
 	
 	Optional<Exercicios> findByDescricao(String descricao);	
 
-
-
-
-
-
-
-
-
-	
 }
